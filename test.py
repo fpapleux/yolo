527806cd98aeccc3fdf5df678a870f05c11833e0
Fabien Papleux <fabien@papleux.com>
2023-01-25 18:03:21 +0100

    pics

--- test.py
+++ test.py
@@ -3,14 +3,12 @@
 import cv2 as cv
 import yolo
 
-original = cv.imread('dog.jpg')
+original = cv.imread('img/IMG_9723.jpg')
 myImage = yolo.Image(original)
 myImage.process()
 print(myImage)
 
-pic = cv.imread(myImage.filename)
 cv.imshow("original picture", original)
 cv.imshow("Processed Picture", myImage.img)
-cv.imshow("Picture from disk", pic)
 cv.waitKey(0)
 
